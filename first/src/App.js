957033b343892e8edfb9507df0c011cc52374891
gqadirdepar <gqadirdepar@gmail.com>
2020-12-09 16:13:31 +0500

    npx create-react-app in first folder

---
+++ first/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hellow World by Ghulam Qadir
+    </div>
+  );
+}
+
+export default App;
